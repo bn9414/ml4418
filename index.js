0b56c10e5f43b4f84fe9c8c05c1fd56627fcbdc9
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-04-17 17:17:29 +0530

    s

--- index.js
+++ index.js
@@ -1,151 +1,85 @@
-//paste this code under the head tag or in a separate js file.
-	// Wait for window load
-	$(window).load(function() {
-		// Animate loader off screen
-		$(".se-pre-con").fadeOut("slow");;
-        	
-    var slidehd= document.getElementsByClassName('slidehd');
-            var slmgj= document.getElementsByClassName('slmgj');
-    
-        
-function portfullf(){
-   slidehd[0].style.opacity="1";
-    slidehd[0].style.left="0px";
-    slmgj[0].className="slmgj slani";
-    
-    
-}
-
-portfullf();
-
-    });
-
-var h = parseInt(window.innerHeight);
-        var w = parseInt(window.innerWidth);
 
 
-var sidehdj=document.getElementsByClassName('sidehdj');
-var mphilhd=document.getElementsByClassName('mphilhd');
 
-var psidehdj=document.getElementById('psidehdj');
-var consj1=document.getElementById('consj1');
-var consj2=document.getElementById('consj2');
-var intj=document.getElementById('intj');
 
-window.onscroll = function() {scrollani()};
+var prev = document.getElementById('prev');
+prev.addEventListener('click',function(e){
+  incre(-1);},false);
+var nxt = document.getElementById('nxt');
+nxt.addEventListener('click',function(e){
+  incre(1);
+    clearInterval(xset);
 
-function scrollani() {
-    /* home*/
- if(w>1000){
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
- sidehdj[0].style.opacity="1";
- sidehdj[0].style.top="130px";
-        
-    
-    }
-     if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
- mphilhd[0].style.opacity="1";
- mphilhd[0].style.top="160px";
-        
-    
-    }
-     
-     
-     if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+},false);
+ var ban = document.getElementsByClassName('ban');
+var caphd = document.getElementsByClassName('hdtxt');
+var j =0;
 
-         consj1.style.opacity="1";
- consj1.style.left="0px";
-      consj2.style.opacity="1";
- consj2.style.left="0px";
-    
-         
-    }
-     
-        if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1500) {
-
-         intj.style.opacity="1";
- intj.style.left="0px";
-      
-    
-         
-    }
-     
-    
-    
- }
-    
-     if(w>880){
-    if (document.body.scrollTop > 1850 || document.documentElement.scrollTop > 1850) {
-
-         psidehdj.style.opacity="1";
- psidehdj.style.top="160px";
-    
-    }
-         
-    
-    
+ function incre(k){
+     j = j + k;
+     if(j == -1){
+         j = ban.length-1;
      }
+     if(j == ban.length){
+         j=0;
+     }
+    scrollImg(j);
+ }
+function scrollImg(z){
+    ban[0].style.display="none";
+    ban[1].style.display="none";
+    ban[2].style.display="none";
+    ban[3].style.display="none";
+    ban[z].style.display="block";
+    
+   caphd[0].style.display="none";
+    caphd[1].style.display="none";
+    caphd[2].style.display="none";
+     caphd[3].style.display="none";
+    caphd[z].style.display="block";                              
+}
+scrollImg(0);
+var xset= setInterval(function(){incre(1);},4000);
 
 
-
-
-
-
-
+/*
+function scrollImg(e){
+    var ban1 = document.getElementById('ban1');
+    var ban2 = document.getElementById('ban2');
+    var caphd = document.getElementById('caphd');
+    
+    if( e == 1){
+        ban1.style.display="none";
+        ban1.style.animationName="fadeIn";
+        ban1.style.animationDuration="0.7s";
+        ban1.style.transistion="0.1s";
+        caphd.style.top="-40px";
+        caphd.style.opacity="0";
+        
+         ban2.style.display="block";
+        ban2.style.transistion="0.1s";
+        ban2.style.animationName="fadeIn";
+        ban2.style.animationDuration="1s";
+        
+        caphd.style.transistionDelay="2s";
+        caphd.style.top="0px";
+        caphd.style.opacity="1";
+        
+        
+        
+    }
+        
+    else if(e == -1){
+         ban1.style.display="block";
+         ban1.style.animationName="fadeIn";
+        ban1.style.animationDuration="0.7s";
+        ban1.style.transistion="0.1s";
+         ban2.style.display="none";
+        ban2.style.transistion="0.1s";
+         ban2.style.animationName="fadeIn";
+        ban2.style.animationDuration="1s";
+        
+    }
 }
 
-
-
-
-
-
-
-
-
-var tswit= document.getElementsByClassName('tswit');
-
-var tsin= document.getElementsByClassName('tsin');
-var tall= document.getElementsByClassName('tall');
-function tswitch(n){
-    
-    tsin[0].style.backgroundColor="#fcfcfc";
-     tsin[1].style.backgroundColor="#fcfcfc";
-     tsin[2].style.backgroundColor="#fcfcfc";
-     
-    tall[0].style.display="none";tall[1].style.display="none";tall[2].style.display="none";
-    
-     tall[n].style.display="block";tsin[n].style.backgroundColor="#454545";
-    
-
-}
-if(w<880){
-tswitch(0);
-}
-tswit[0].addEventListener('click',function() {tswitch(0);}, false);
-
-tswit[1].addEventListener('click',function() {tswitch(1);}, false);
-
-tswit[2].addEventListener('click',function() {tswitch(2);}, false);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
